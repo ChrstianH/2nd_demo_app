645f85d8109feed934946e3cd891e13e3f547e8f
ChrstianH <henschel_christian@web.de>
2026-03-24 12:16:28 +0100

    added InvestmentResultsComponent; outsorced models and a service

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,27 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
-
 export class UserInputComponent {
-  @Output() calculate = new EventEmitter<{ initialInvestment: number; annualInvestment: number; expectedReturn: number; duration: number }>();
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
-  enteredDuration= '10';
+  enteredDuration = '10';
+
+  constructor(private investmentService: InvestmentService) {}
 
   onSubmit(): void {
-    return this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment,
       annualInvestment: +this.enteredAnnualInvestment,
       expectedReturn: +this.enteredExpectedReturn,
-      duration: +this.enteredDuration
+      duration: +this.enteredDuration,
     });
+    this.enteredInitialInvestment = '0';
+    this.enteredAnnualInvestment = '0';
+    this.enteredExpectedReturn = '5';
+    this.enteredDuration = '10';
   }
 }
